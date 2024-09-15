a8952da7a68613a23a7a8770bdf4271ea44c7915
Sak-drago <sakshatsachdeva@gmail.com>
2024-09-15 18:31:45 +0530

    Task Update: RFC commented in

--- OS_Assignment_1/q3/date.c
+++ OS_Assignment_1/q3/date.c
@@ -16,7 +16,7 @@ int main(int argc, char *argv[]){
       utc();
     }
     else if(strcmp(argv[1], "-r") == 0){
-      //rfc();
+      rfc();
     }
     else printf("Use either -u or -r\n");
   }
@@ -37,4 +37,12 @@ void default_time(){
   time_t t = time(NULL);
   struct tm tm = *localtime(&t);
   printf("%d-%02d-%02d %02d:%02d:%02d\n", tm.tm_year+1900,tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec); 
+}
+
+void rfc(){
+  time_t t = time(NULL);
+  struct tm tm = *gmtime(&t);
+  char *wday[] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+  char *mon[] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+  printf("%s, %02d %s %04d %02d:%02d:%02d +0000\n", wday[tm.tm_wday], tm.tm_mday, mon[tm.tm_mon], tm.tm_year + 1900, tm.tm_hour, tm.tm_min, tm.tm_sec);
 }
